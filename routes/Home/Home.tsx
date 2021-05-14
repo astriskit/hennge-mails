470744d90ca9eb67b2e326053fc3875c9461491f
astriskit <harry2office@gmail.com>
2021-05-14 12:19:28 +0530

    added noResults

--- routes/Home/Home.tsx
+++ routes/Home/Home.tsx
@@ -14,21 +14,33 @@ export const Home = ({ emails = [] }: { emails: Email[] }) => (
   <div className={styles.home}>
     <DateRange className={styles.dateRange} />
     <Results className={styles.results}>{emails.length}</Results>
-    <TableHeader className={styles.mobileTableHeader} />
-    <div className={styles.emailList}>
-      {emails.map(({ to, from, subject, date, body }, id) => (
-        <MobileEmailCard
-          key={id}
-          className={styles.mobileEmailCard}
-          to={to}
-          from={from}
-          subject={subject}
-          date={date}
-        >
-          {body}
-        </MobileEmailCard>
-      ))}
-    </div>
-    <EmailTable emails={emails} className={styles.emailTable} />
+    {emails.length && <TableHeader className={styles.mobileTableHeader} />}
+    {emails.length && (
+      <div className={styles.emailList}>
+        {emails.map(({ to, from, subject, date, body }, id) => (
+          <MobileEmailCard
+            key={id}
+            className={styles.mobileEmailCard}
+            to={to}
+            from={from}
+            subject={subject}
+            date={date}
+          >
+            {body}
+          </MobileEmailCard>
+        ))}
+      </div>
+    )}
+    {emails.length && (
+      <EmailTable emails={emails} className={styles.emailTable} />
+    )}
+    {!emails.length && (
+      <div className={styles.noResults}>
+        <hr />
+        <div>
+          <img src="/images/logo.png" alt="logo" />
+        </div>
+      </div>
+    )}
   </div>
 )
